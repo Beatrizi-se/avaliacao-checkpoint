a029d5e0e2de35713ec0a8eea55309287733bddd
Ana Beatriz <anasoterobetriz@gmail.com>
2026-03-30 15:09:32 -0300

    feat: adiciona estrutura inicial do app UseDev

---
+++ usedev_uninassau/android/app/src/main/kotlin/br/dev/bea/usedev_uninassau/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.dev.bea.usedev_uninassau
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ usedev_uninassau/lib/src/screens/initial_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:usedev_uninassau/src/widgets/banner_hero_widget.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  _InitialScreenState createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu_outlined, size: 32),
+        actions: [
+          Icon(Icons.person_outlined, size: 32),
+          SizedBox(width: 25),
+          Icon(Icons.shopping_cart_outlined, size: 32),
+          SizedBox(width: 25),
+        ],
+        title: Image.asset(
+          'assets/logo_usedev.png',
+          height: 40,
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView( 
+        child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const BannerHeroWidget(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Column(
+              children: [
+                Text(
+                  'Categorias',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.orbitron(
+                    fontSize: 31,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 12),
+                Text(
+                  'De roupas a gadgets tecnológicos temos tudo para atender suas paixões e hobbies com estilo e autenticidade',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ],
+      )
+      )
+    );
+  }
+}
